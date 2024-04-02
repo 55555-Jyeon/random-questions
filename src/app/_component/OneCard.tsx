9744e33a9803db9cf5957e7641d01a4a14dcd9a2
kiminn <134191815+kiminn@users.noreply.github.com>
2024-04-03 02:53:54 +0900

    css complete

--- src/app/_component/OneCard.tsx
+++ src/app/_component/OneCard.tsx
@@ -1,19 +1,43 @@
-"use client";
+'use client';
 
-import styles from "./oneCard.module.scss";
-import { questions } from "../../../mocks/QuestionList";
-import { QuestionItem, TProps, Tquestions } from "../../../types/type";
-import { useEffect, useState } from "react";
+import styles from './oneCard.module.scss';
+import { TProps } from '../../../types/type';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+import { CardActionArea } from '@mui/material';
+import { useState } from 'react';
 
 export default function OneCard({ idx, category, question }: TProps) {
-  const handleShowCard = (e: React.MouseEvent<HTMLDivElement>) => {
-    console.log(e.currentTarget);
-  };
+    const [isFlipped, setIsFlipped] = useState(false);
+    const handleShowCard = (e: React.MouseEvent<HTMLDivElement>) => {
+        setIsFlipped(!isFlipped);
+    };
 
-  return (
-    <div key={idx} onClick={handleShowCard}>
-      <div>{category}</div>
-      <div>{question}</div>
-    </div>
-  );
+    return (
+        <Grid item key={idx} className={styles.card}>
+            <CardActionArea>
+                <Card className={styles.flipper} onClick={handleShowCard} sx={{ width: 230, height: 230 }}>
+                    {!isFlipped ? (
+                        <div className={styles.front}>
+                            <h1>{idx + 1}</h1>
+                        </div>
+                    ) : (
+                        ''
+                    )}
+                    <div className={styles.back}>
+                        <CardContent className={styles.innerContainer}>
+                            <Typography variant="body2" color="text.secondary">
+                                {category}
+                            </Typography>
+                            <Typography className={styles.text} gutterBottom variant="h6" component="div">
+                                {question}
+                            </Typography>
+                        </CardContent>
+                    </div>
+                </Card>
+            </CardActionArea>
+        </Grid>
+    );
 }
