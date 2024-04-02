53e2613d371af1d81c09295f85764af5ba6bd61c
55555-Jyeon <jyeon380516@gmail.com>
2024-04-03 00:11:33 +0900

    folder structure complete

---
+++ src/app/_component/OneCard.tsx
@@ -0,0 +1,5 @@
+import styles from "./oneCard.module.scss";
+
+export default function OneCard() {
+  return <div></div>;
+}
---
+++ types/type.ts
@@ -0,0 +1,9 @@
+export type Tquestions = {
+  CS: string[];
+  TS: string[];
+  JS: string[];
+  "HTML/CSS": string[];
+  React: string[];
+  project: string[];
+  basics: string[];
+};
